b5dade2cbdd4db3fae244f1fab8f27bbdba46c29
trOOnies <facundomartinscasso@gmail.com>
2024-10-03 19:04:02 -0300

    add cropper_swarm_name to batch_crop

--- app/backbone/routers/processes/cropping.py
+++ app/backbone/routers/processes/cropping.py
@@ -3,7 +3,7 @@
 from fastapi import APIRouter, status
 import requests
 
-from backbone.endpoints import parse_or_raise
+from backbone.endpoints import parse_or_raise, mlendp
 from backbone.options import ML_ENDPOINTS as MLEP
 
 router = APIRouter()
@@ -11,6 +11,7 @@ router = APIRouter()
 
 @router.post("/batch", status_code=status.HTTP_201_CREATED)
 def batch_crop(
+    cropper_swarm_name: str,
     move: bool = True,
     use_croppers: list[str] | None = None,
     use_fmts: list[str] | None = None,
@@ -18,18 +19,22 @@ def batch_crop(
     """[NEW] Run all Croppers iterating on images first.
 
     move: Whether to move the source images at the end of the cropping.
-        Note: The MovableReport still avoid creating crops
+        Note: The MovableReport still avoids creating crops
         of duplicate source images.
 
     Filter options (cannot be used at the same time):
     - use_croppers: Filter cropping using Cropper names (level=Cropper).
     - use_fmt: Filter cropping using FullModelTypes names (level=crop type).
     """
+    # TODO: Go first to the DB to assert it's already registered
     return parse_or_raise(
         requests.post(
-            MLEP.CROP,
-            json={
+            mlendp(f"{MLEP.CROP}/batch"),
+            params={
+                "cropper_swarm_name": cropper_swarm_name,
                 "move": move,
+            },
+            json={
                 "use_croppers": use_croppers,
                 "use_fmts": use_fmts,
             },
