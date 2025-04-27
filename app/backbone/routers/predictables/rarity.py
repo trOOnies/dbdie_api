d000c03c01155f2eadc1d5d03be7f5613f5feb62
trOOnies <facundomartinscasso@gmail.com>
2025-04-27 03:58:48 -0300

    add create rarity endpoint

--- app/backbone/routers/predictables/rarity.py
+++ app/backbone/routers/predictables/rarity.py
@@ -1,17 +1,23 @@
 """Router for item ratity."""
 
-from dbdie_classes.schemas.types import RarityOut
+from dbdie_classes.schemas.types import RarityCreate, RarityOut
 from fastapi import APIRouter, Depends, status
 from typing import TYPE_CHECKING
 
 from backbone.database import get_db
 from backbone.endpoints import (
+    NOT_WS_PATT,
+    add_commit_refresh,
     delete_one,
     do_count,
     filter_one,
     get_many,
+    get_req,
+    getr,
 )
+from backbone.exceptions import ValidationException
 from backbone.models.types import Rarity
+from backbone.options import ENDPOINT as EP
 
 if TYPE_CHECKING:
     from sqlalchemy.orm import Session
@@ -42,7 +48,18 @@ def get_item(id: int, db: "Session" = Depends(get_db)):
     return filter_one(db, Rarity, id)[0]
 
 
-# TODO: Create rarity
+@router.post("", response_model=RarityOut, status_code=status.HTTP_201_CREATED)
+def create_rarity(rarity: RarityCreate, db: "Session" = Depends(get_db)):
+    """Create a DBD rarity tier."""
+    if NOT_WS_PATT.search(rarity.name) is None:
+        raise ValidationException("Rarity name can't be empty")
+
+    new_rarity = rarity.model_dump() | {"id": getr(f"{EP.RARITY}/count")}
+    new_rarity = Rarity(**new_rarity)
+
+    add_commit_refresh(db, new_rarity)
+
+    return get_req(EP.RARITY, new_rarity.id)
 
 
 @router.delete("/{id}", status_code=status.HTTP_200_OK)
